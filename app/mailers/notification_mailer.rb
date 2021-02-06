925d9d13fe3d7ab433915ff2f5c734e4ff6e5672
seeya1127 <seeya11271995@gmail.com>
2021-02-06 06:16:16 +0000

    メール認証完成

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,9 @@
+class NotificationMailer < ApplicationMailer
+  default from: ENV['email']
+
+  def complete_mail(user)
+    @user = user
+    #p "##email:#{@user.email}"
+    mail(subject: "COMPLETE join your address" ,to: @user.email)
+  end
+end
