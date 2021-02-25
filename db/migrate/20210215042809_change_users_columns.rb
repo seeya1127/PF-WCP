a627a259d3ba36d893555382f02d114be30f4ac6
EC2 Default User <ec2-user@ip-172-31-22-41.ap-northeast-1.compute.internal>
2021-02-25 10:20:28 +0000

    デプロイ

--- db/migrate/20210215042809_change_users_columns.rb
+++ db/migrate/20210215042809_change_users_columns.rb
@@ -1,6 +1,5 @@
 class ChangeUsersColumns < ActiveRecord::Migration[5.2]
   def change
-    remove_column :users, :profile_image, :text
-    # add_column :users, :profile_image_id, :string
+    add_column :users, :profile_image_id, :string
   end
 end
